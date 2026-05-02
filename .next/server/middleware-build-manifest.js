1090f6d26c0b51640adf764fbc202cca1e1533c6
shohagfrominoodex <shohag@inoodex.com>
2026-05-02 19:05:09 +0600

    NOTE: old file created

--- .next/server/middleware-build-manifest.js
+++ .next/server/middleware-build-manifest.js
@@ -7,15 +7,15 @@ globalThis.__BUILD_MANIFEST = {
     "static/chunks/03~yq9q893hmn.js"
   ],
   "lowPriorityFiles": [
-    "static/mOxb2zDSDeJcctXL2F-MZ/_buildManifest.js",
-    "static/mOxb2zDSDeJcctXL2F-MZ/_ssgManifest.js",
-    "static/mOxb2zDSDeJcctXL2F-MZ/_clientMiddlewareManifest.js"
+    "static/vW6iQafiZhIyj0oOcuXBX/_buildManifest.js",
+    "static/vW6iQafiZhIyj0oOcuXBX/_ssgManifest.js",
+    "static/vW6iQafiZhIyj0oOcuXBX/_clientMiddlewareManifest.js"
   ],
   "rootMainFiles": [
     "static/chunks/045g~0~zva_mv.js",
     "static/chunks/0dgq26a5_oy.a.js",
     "static/chunks/0hstpqahkt253.js",
-    "static/chunks/0257pdz1-imal.js",
-    "static/chunks/turbopack-06pdccoitlj0e.js"
+    "static/chunks/0oui10gukf8nf.js",
+    "static/chunks/turbopack-0-q9yn3_t3nu7.js"
   ]
 };
